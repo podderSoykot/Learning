e155b369a47fe48a63a9b8bf9af17bc428692094
podderSoykot <diptopodder95@gmail.com>
2025-08-28 13:33:22 +0600

    learning Map

---
+++ Data_Structure/map.py
@@ -0,0 +1,12 @@
+numbers=[1,2,3,4,5,6,7]
+
+def sequence(x):
+    return x**2
+
+square_number=map(sequence,numbers)
+print(list(square_number))
+
+#map using lambda 
+
+square_number2=map(lambda x:x **3,numbers)
+print(list(square_number2))
